9a451679a47e239e75f7e45234b5d01523ba944b
Christoffer Sladin <102432146+ChristofferSladin@users.noreply.github.com>
2023-03-27 16:55:35 +0200

    added template

--- Assignment-WebBank/Pages/Index.cshtml.cs
+++ Assignment-WebBank/Pages/Index.cshtml.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Assignment_WebBank.BankAppData;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.CompilerServices;
 
 namespace Assignment_WebBank.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly BankAppDataContext _dbContext;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, BankAppDataContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
         public void OnGet()
