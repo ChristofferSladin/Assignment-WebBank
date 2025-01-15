cf157d7b9e27aee2593a7e0dc974334ceb4a01b0
chris <christoffer.sladin@hotmail.com>
2025-01-15 21:25:29 +0100

    is development clause back to default

--- Assignment-WebBank/Program.cs
+++ Assignment-WebBank/Program.cs
@@ -23,11 +23,7 @@ builder.Services.AddResponseCaching();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
